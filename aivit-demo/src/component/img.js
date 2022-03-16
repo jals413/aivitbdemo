7fe47289b81e227455805946fc0583236eed0e29
Jal Thakor <jalsthakor413@gmail.com>
2022-03-16 12:04:31 +0530

    source_code

---
+++ aivit-demo/src/component/img.js
@@ -0,0 +1,15 @@
+import "./logo.css"
+
+function Logo() {
+    return(
+        <>
+        <div className="logo">
+            <img src="https://media.discordapp.net/attachments/941554263128150036/945755502460153916/vitb.png?width=1440&height=540" alt=""/>
+            <img src="https://media.discordapp.net/attachments/941554263128150036/945755478292578435/aic.png?width=981&height=701" alt="" />
+            <img src="https://media.discordapp.net/attachments/941554263128150036/945755494327398490/aiml.png?width=1440&height=473" alt="" />
+        </div>
+        </>
+    )
+}
+export default Logo;
+
---
+++ aivit-demo/src/component/nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./nav.css"
+function Nav() {
+    return (<>
+        <div className="nav">
+            <ul className="ul">
+                <li className="item">Home</li>
+                <li className="item">Sessions</li>
+                <li className="item">Sponsors</li>
+                <li className="item">Our Teams</li>
+                <li className="item">Brochure</li>
+            </ul>
+        </div>
+    </>
+    )
+}
+
+export default Nav;
